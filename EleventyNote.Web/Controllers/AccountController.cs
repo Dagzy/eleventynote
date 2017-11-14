884ce25e2341cbb3328773cb4ec3e365aead357d
Dagzy <quincye.williams@gmail.com>
2017-11-14 15:24:03 -0500

    QW added references

--- EleventyNote.Web/Controllers/AccountController.cs
+++ EleventyNote.Web/Controllers/AccountController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using EleventyNote.Web.Models;
+using EleventyNote.Data;
 
 namespace EleventyNote.Web.Controllers
 {
@@ -151,7 +152,7 @@ namespace EleventyNote.Web.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ElevenNoteUser { UserName = model.Email, Email = model.Email };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
@@ -367,7 +368,7 @@ namespace EleventyNote.Web.Controllers
                 {
                     return View("ExternalLoginFailure");
                 }
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ElevenNoteUser { UserName = model.Email, Email = model.Email };
                 var result = await UserManager.CreateAsync(user);
                 if (result.Succeeded)
                 {
