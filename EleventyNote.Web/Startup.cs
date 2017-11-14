f5fdcbe5d10323be7ab3f1f95e57c7bc033191d8
Dagzy <quincye.williams@gmail.com>
2017-11-14 14:18:05 -0500

    QW initial commit

---
+++ EleventyNote.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EleventyNote.Web.Startup))]
+namespace EleventyNote.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
