812a00092b7e2c9869fc4cacaf08b7ea76be75d4
Quincy Williams <quincye.williams@gmail.com>
2017-11-15 16:41:38 -0500

    QW migrated data, added create view and method

---
+++ EleventyNote.Data/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace EleventyNote.Data.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<EleventyNote.Data.ElevenNoteDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "EleventyNote.Data.ElevenNoteDbContext";
+        }
+
+        protected override void Seed(EleventyNote.Data.ElevenNoteDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
---
+++ EleventyNote.Web/Controllers/NotesController.cs
@@ -0,0 +1,45 @@
+﻿using EleventyNote.Models;
+using EleventyNote.Services;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EleventyNote.Web.Controllers
+{
+    [Authorize]
+    public class NotesController : Controller
+    {
+        // GET: Notes
+        public ActionResult Index()
+        {
+            //var userId = Guid.Parse(User.Identity.GetUserId()); Pass this to svc if it's screwed
+            var svc = new NoteService(Guid.Parse(User.Identity.GetUserId()));
+            var model = svc.GetNotes();
+            return View(model);
+        }
+        public ActionResult Create()
+        {
+            var model = new NoteCreateModel();
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(NoteCreateModel model)
+        {
+            if (!ModelState.IsValid) return View(model);
+
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var svc = new NoteService(userId);
+
+            if (!svc.CreateNote(model))
+            {
+                ModelState.AddModelError("", "Unable to create note");
+                return View(model);
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
